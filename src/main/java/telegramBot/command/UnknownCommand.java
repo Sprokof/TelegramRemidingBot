d0d098ce16924ae62a3fa6913dd21140c40f2940
Sergei Prokofev <hergod@outlook.com>
2022-01-06 22:57:15 +0400

    repeated edit work on SendNotice class

--- src/main/java/telegramBot/command/UnknownCommand.java
+++ src/main/java/telegramBot/command/UnknownCommand.java
@@ -4,8 +4,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import telegramBot.service.SendMessageService;
 
 public class UnknownCommand implements Command{
-    public static final String UNKNOWN_COMMAND = "Я понимаю команды '/START', '/STOP', " +
-            "'/ADD'/, '/RESTART'";
+    public static final String UNKNOWN_COMMAND = "Я понимаю команды '/start', '/stop', " +
+            "'/add'/, '/restart'";
     private final SendMessageService sendMessageService;
 
     public UnknownCommand(SendMessageService sendMessageService){
