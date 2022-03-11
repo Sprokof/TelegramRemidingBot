ac0cf1f18b41c6127080140ad069c1dab6eb91a8
Sergei Prokofev <fogg8341@gmail.com>
2022-03-11 13:40:49 +0400

    Delete src/main/java/telegramBot/hidenPackage directory

--- src/main/java/telegramBot/hidenPackage/dao/Dao.java
+++
@@ -1,8 +0,0 @@
-package telegramBot.hidenPackage.dao;
-
-import telegramBot.hidenPackage.entity.RemindDPer;
-
-public interface Dao {
-    RemindDPer getRemindById(int id);
-    boolean updateRemind(RemindDPer remindDPer);
-}
