057189474da0cfcae9726945463ea28091daa673
Sergei Prokofev <hergod@outlook.com>
2022-03-12 22:13:05 +0400

    append .gitignore

--- src/main/java/telegramBot/entity/Message.java
+++ src/main/java/telegramBot/entity/Message.java
@@ -1,14 +1,26 @@
 package telegramBot.entity;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
-public class Message {
-    private String chatId;
+@Entity
+@NoArgsConstructor
+public class Message{
+    @Id
+    private int id;
+    private  String chatId;
     private Integer messageId;
 
+
+    public Message(String chatId, Integer messageId){
+        this.messageId = messageId;
+        this.chatId = chatId;
+
+    }
 }
