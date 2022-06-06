6bd72317521637357464728bb8344007b1fa79b1
Sergei Prokofev <hergod@outlook.com>
2022-06-06 13:20:20 +0400

    create env variables for bot values in properties file

--- src/test/java/XORCryptTest.java
+++ src/test/java/XORCryptTest.java
@@ -1,20 +1,20 @@
 import org.junit.Test;
 import telegramBot.crypt.XORCrypt;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class XORCryptTest {
 
     @Test
     public void code(){
-        String FOR_TEST_STRING = "FOR_TEST_STRING";
+        final String  FOR_TEST_STRING = "FOR_TEST_STRING";
         String key = XORCrypt.keyGenerate();
         String s = XORCrypt.encrypt(FOR_TEST_STRING, key);
 
         String result = XORCrypt.
                decrypt(XORCrypt.
                        stringToIntArray(s), key);
-        assertEquals(true, FOR_TEST_STRING.equals(result));
+        assertTrue( FOR_TEST_STRING.equals(result));
     }
 
 }
